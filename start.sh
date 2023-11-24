9d63b6811c7edc6a397f12c4b838a39c35568b7c
D S I GUNASEKARA <148281527+shamitum@users.noreply.github.com>
2023-11-25 02:24:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ cd /xray
 apk update
 apk add --no-cache wget unzip
 wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-wget https://raw.githubusercontent.com/shamitum/xray-docker-custom-config/main/config.json
 unzip ./Xray-linux-64.zip
 rm ./Xray-linux-64.zip
 ./xray -c ./config.json
